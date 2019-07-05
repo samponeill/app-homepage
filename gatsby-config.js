06912ba783f28d9c7244cb0f3b4a493495c5c86c
Sam O'Neill <sam@samponeill.com>
2019-07-05 12:39:45 +0100

    Changed name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,13 +47,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `jamjar-starter-default`,
+        short_name: `jamjar`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
