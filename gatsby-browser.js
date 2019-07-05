1af66c86c7458ded700c38e5e5ba00d7de64c8c3
Sam O'Neill <sam@samponeill.com>
2019-07-05 11:05:45 +0100

    Removed polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,10 +11,10 @@ import "./src/scss/style.scss";
 // ES5 way
 // exports.onClientEntry = () => {
 // ES6 way
-export const onClientEntry = () => {  
+//export const onClientEntry = () => {  
     // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-    if (typeof window.IntersectionObserver === `undefined`) {
-      import(`intersection-observer`)
-      console.log(`# IntersectionObserver is polyfilled!`)
-    }
-}
+//    if (typeof window.IntersectionObserver === `undefined`) {
+//      import(`intersection-observer`)
+//      console.log(`# IntersectionObserver is polyfilled!`)
+//    }
+//}
