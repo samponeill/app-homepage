f631e6c1ac07cc3edb255952ac3ea6342a8e0afc
Sam O'Neill <sam@samponeill.com>
2019-07-17 12:45:23 +0100

    Changed 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,8 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <section>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Oops</h1>
+      <p>You just hit a route that doesn&#39;t exist ... the sadness</p>
     </section>
   </Layout>
 )
